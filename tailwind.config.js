f850de84df41dba094d11155c1c49ed7b66650a2
Shubhang Chourasia <70596665+shubhangchourasia@users.noreply.github.com>
2022-04-11 10:31:39 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "logo-green-1": "#03DE74",
+        "logo-green-2": "#83FFC0",
+        "logo-blue": "#58FFE6",
+        "text-green-3": "#55FFCC",
+        "background-blue": "#0B1F52",
+      },
+    },
+    // screens: {
+    //   xl: { max: "1920px" },
+
+    //   lg: { max: "1280px" },
+
+    //   md: { max: "768px" },
+
+    //   sm: { max: "425px" },
+    // },
+  },
+  plugins: [],
+};
